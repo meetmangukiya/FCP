0ddbb995e47a265a0774b77349f50e504eec707f
Meet Mangukiya <meet123mangukiya@gmail.com>
2017-03-04 13:18:08 +0530

    Create pattern.c

---
+++ pattern.c
@@ -0,0 +1,27 @@
+/**
+ to print a pattern like:
+ 54321
+  4321
+   321
+    21
+     1
+ if the input is 5
+ */
+ 
+#include <stdio.h>
+#include <conio.h>
+
+void main() {
+    clrscr();
+    int rows, i , j;
+    
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("\n");
+    for(i = 1; i <= rows; i++) {
+        for(j = 0; j < i - 1; j++) printf(" ");
+        for(j = rows - i + 1; j >= 1 ; j--) printf("%d", j);
+        printf("\n");
+    }
+    getch();
+}
