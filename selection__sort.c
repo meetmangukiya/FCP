cc4f74b7ad526d5be8e1be36c1c8e5ec221fc866
Meet Mangukiya <meet123mangukiya@gmail.com>
2017-03-11 23:07:54 +0530

    Add selection__sort.c
    Closes #1

---
+++ selection__sort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Length of array: ");
+    scanf("%d", &n);
+    int ar[n];
+
+    printf("\nEnter %d numbers: ", n);
+    for(i = 0; i < n; i++) scanf("%d", &ar[i]); // Populating the array
+
+    for(i = 0; i < n; i++) {
+        int min, j, li = -1, tmp;
+
+        min = ar[i];
+        for(j = i; j < n; j++) {
+            if (ar[j] < min) {
+                min = ar[j];
+                li = j;
+            }
+        }
+
+        // Swapping
+        if (li != -1) {
+            tmp = ar[i];
+            ar[i] = min;
+            ar[li] = tmp;
+        }
+    }
+
+    printf("\nSorted array: ");
+    for(i = 0; i < n; i++) printf("%d ", ar[i]);
+
+    return 0;
+}
